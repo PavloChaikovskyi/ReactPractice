f8c13f4f31a8c04916cc09faa9986fb0676c2d9e
Pavlo <chaykovskiypavel@gmail.com>
2021-12-12 22:55:09 +0100

    created conrolable / uncontrolable components

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
-import React, { useState } from "react"; // add to every file with component
+import React, { useState, useRef } from "react"; // add to every file with component
 // import Counter from "./components/Counter";
 // import ClassCounter from "./components/ClassCounter";
 import './styles/App.css';
 // import PostItem from "./components/PostItem"
 import PostList from "./components/PostList";
 import MyButton from "./components/UI/button/MyButton";
+import MyInput from "./components/UI/input/MyInput";
 
 function App() {
   const [posts, setPosts] = useState([
@@ -13,12 +14,32 @@ function App() {
     {id: 3, title: 'Php', body: 'Desc'},
   ])
 
+  const [title, setTitle] = useState('')
+  const bodyInputRef = useRef(); 
+  const addNewPost = (e) => {
+    e.preventDefault();
+    console.log(title);
+    console.log(bodyInputRef.current.value);
+  }
+
   return (
     <div className="App">      
     <form action="">
-        <input type="text" placeholder="Post Name" />
-        <input type="text" placeholder="Post Desc" />
-        <MyButton>Create post</MyButton>
+
+        {/* Управляемий компонент / Controlable component */}
+        <MyInput
+            value={title}
+            onChange={e => setTitle(e.target.value)}
+            type="text"
+            placeholder="Post Name"
+          />
+          {/* Неуправляемий инпут / Uncontrolable input  */}
+        <MyInput 
+          ref={bodyInputRef}
+          type="text" 
+          placeholder="Post Desc"
+         />
+        <MyButton onClick={addNewPost}>Create post</MyButton>
       </form>  
       <PostList posts={posts} title={'Post List 1'}/>
      </div>
