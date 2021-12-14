16ea473abd9ee9cab87aa64a0b3926be6a75e92f
Pavlo <chaykovskiypavel@gmail.com>
2021-12-14 23:39:59 +0100

    added Search functionality

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
-import React, { useState } from "react"; // add to every file with component
+import React, { useMemo, useState } from "react"; // add to every file with component
 // import Counter from "./components/Counter";
 // import ClassCounter from "./components/ClassCounter";
 import './styles/App.css';
 // import PostItem from "./components/PostItem"
 import PostList from "./components/PostList";
 // import MyButton from "./components/UI/button/MyButton";
-// import MyInput from "./components/UI/input/MyInput";
+import MyInput from "./components/UI/input/MyInput";
 import PostForm from "./components/PostForm";
 import MySelect from "./components/UI/select/MySelect";
 
@@ -17,6 +17,20 @@ function App() {
   ])
 
   const [selectedSort, setSelectedSort] = useState('')
+  const [searchQuery, setSearchQuery] = useState('')
+
+  const sortedPosts = useMemo(() => {
+    if(selectedSort) {
+      console.log('Func SORTEDPOSTS DID JOB');
+      return [...posts].sort((a,b) => a[selectedSort].localeCompare(b[selectedSort]))
+    }
+    return posts;
+  }, [selectedSort, posts])
+
+  const sortedAndSearchedPosts = useMemo(() => {
+    return sortedPosts.filter(post => post.title.toLowerCase().includes(searchQuery))
+  }, [searchQuery, sortedPosts])
+
   const createPost = (newPost) => {
     setPosts( [...posts, newPost])
   }
@@ -30,7 +44,6 @@ function App() {
 
   const sortPosts = (sort) => {
     setSelectedSort(sort)
-    setPosts([...posts].sort((a,b) => a[sort].localeCompare(b[sort])))
   } 
 
   return (
@@ -38,7 +51,11 @@ function App() {
       <PostForm create={createPost} />
         <hr style={{margin: '15px 0'}}/>
       <div>
-        <input type="text" />
+      <MyInput
+        value={searchQuery}
+        onChange={e=>setSearchQuery(e.target.value)}
+        placeholder="search..."
+      />
       <MySelect 
         value={selectedSort}
         onChange={sortPosts}
@@ -49,9 +66,9 @@ function App() {
         ]}
       />
       </div>
-      {posts.length
+      {sortedAndSearchedPosts.length
       ? 
-      <PostList remove={removePost} posts={posts} title={'Post List 1'}/>
+      <PostList remove={removePost} posts={sortedAndSearchedPosts} title={'Post List 1'}/>
       :
       <h1 style={{textAlign: 'center'}}>Post List is Empty</h1>
       }
