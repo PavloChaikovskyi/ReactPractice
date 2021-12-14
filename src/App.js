9f96d5f612b2ed074a39821cca2a11719ba5e1e3
Pavlo <chaykovskiypavel@gmail.com>
2021-12-15 00:18:28 +0100

    transform search and sort to one component!!!

--- src/App.js
+++ src/App.js
@@ -4,68 +4,50 @@ import React, { useMemo, useState } from "react"; // add to every file with comp
 import './styles/App.css';
 // import PostItem from "./components/PostItem"
 import PostList from "./components/PostList";
+import PostFilter from "./components/PostFilter";
 // import MyButton from "./components/UI/button/MyButton";
-import MyInput from "./components/UI/input/MyInput";
+// import MyInput from "./components/UI/input/MyInput";
 import PostForm from "./components/PostForm";
-import MySelect from "./components/UI/select/MySelect";
+// import MySelect from "./components/UI/select/MySelect";
 
 function App() {
   const [posts, setPosts] = useState([
-    {id: 1, title: 'JS', body: 'JS Desc'},
-    {id: 2, title: 'Go', body: 'Go Desc'},
+    {id: 1, title: 'JS', body: 'Go Desc'},
+    {id: 2, title: 'Go', body: 'Js Desc'},
     {id: 3, title: 'Php', body: 'Php Desc'},
   ])
 
-  const [selectedSort, setSelectedSort] = useState('')
-  const [searchQuery, setSearchQuery] = useState('')
+  const [filter, setFilter] = useState({sort:'', query: ''})
 
   const sortedPosts = useMemo(() => {
-    if(selectedSort) {
+    if(filter.sort) {
       console.log('Func SORTEDPOSTS DID JOB');
-      return [...posts].sort((a,b) => a[selectedSort].localeCompare(b[selectedSort]))
+      return [...posts].sort((a,b) => a[filter.sort].localeCompare(b[filter.sort]))
     }
     return posts;
-  }, [selectedSort, posts])
+  }, [filter.sort, posts])
 
   const sortedAndSearchedPosts = useMemo(() => {
-    return sortedPosts.filter(post => post.title.toLowerCase().includes(searchQuery))
-  }, [searchQuery, sortedPosts])
+    return sortedPosts.filter(post => post.title.toLowerCase().includes(filter.query))
+  }, [filter.query, sortedPosts])
 
   const createPost = (newPost) => {
     setPosts( [...posts, newPost])
   }
-
 
   // get post from child component 
   const removePost = (post) => {
     setPosts(posts.filter(p => p.id !== post.id))
   }
 
-
-  const sortPosts = (sort) => {
-    setSelectedSort(sort)
-  } 
-
   return (
     <div className="App">      
       <PostForm create={createPost} />
-        <hr style={{margin: '15px 0'}}/>
-      <div>
-      <MyInput
-        value={searchQuery}
-        onChange={e=>setSearchQuery(e.target.value)}
-        placeholder="search..."
+      <hr style={{margin: '15px 0'}}/>
+      <PostFilter 
+        filter={filter}  
+        setFilter={setFilter}
       />
-      <MySelect 
-        value={selectedSort}
-        onChange={sortPosts}
-        defaultValue="Sort by"
-        options={[
-          {value: 'title', name: 'By name'},
-          {value: 'body', name: 'By desc'},
-        ]}
-      />
-      </div>
       {sortedAndSearchedPosts.length
       ? 
       <PostList remove={removePost} posts={sortedAndSearchedPosts} title={'Post List 1'}/>
