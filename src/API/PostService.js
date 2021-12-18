e5775b40852613b651d4313ae9089fee6ef80daf
Pavlo <chaykovskiypavel@gmail.com>
2021-12-19 00:13:42 +0100

    showed fetching error with hook

--- src/API/PostService.js
+++ src/API/PostService.js
@@ -1,12 +1,10 @@
 import axios from "axios";
 
 export default class PostService {
-    static async getAll() {
-       try {
-        const response = await axios.get('https://jsonplaceholder.typicode.com/posts');
-        return response.data
-       } catch (error) {
-           console.log('error:', error)
-       }
-    }
+  static async getAll() {
+    const response = await axios.get(
+      "https://jsonplaceholder.typicode.com/posts"
+    );
+    return response.data;
+  }
 }
