2433a804dfb8eaaeff54e90e87f9a3fbdf70c19c
Pavlo <chaykovskiypavel@gmail.com>
2021-12-20 01:40:02 +0100

    pagination : visual + added part of functionality

--- src/App.js
+++ src/App.js
@@ -15,15 +15,25 @@ import { usePosts } from "./hooks/usePosts";
 import PostService from "./API/PostService";
 import Loader from "./components/UI/loader/Loader";
 import { useFetching } from "./hooks/useFetching";
+import { getPageCount, getPagesArray } from "./components/utils/pages";
+
 
 function App() {
   const [posts, setPosts] = useState([])
   const [filter, setFilter] = useState({sort:'', query: ''})
   const [modal, setModal] = useState(false)
+  const [totalPages, setTotalPages] = useState(0)
+  const [limit, setLimit] = useState(10)
+  const [page, setPage] = useState(1)
   const sortedAndSearchedPosts = usePosts(posts, filter.sort, filter.query)
+
+  let pagesArray = getPagesArray(totalPages)
+
   const [fetchPosts, isPostsLoading, postError  ] = useFetching( async () => {
-    const posts = await PostService.getAll()
-    setPosts(posts)
+    const response = await PostService.getAll(limit, page)
+    setPosts(response.data)
+    const totalCount = response.headers['x-total-count']
+    setTotalPages(getPageCount(totalCount, limit))
   })
 
   useEffect(() => {
@@ -38,6 +48,11 @@ function App() {
   // get post from child component 
   const removePost = (post) => {
     setPosts(posts.filter(p => p.id !== post.id))
+  }
+
+  const changePage = (page) => {
+    setPage(page)
+    fetchPosts()
   }
 
   return (
@@ -63,7 +78,19 @@ function App() {
       ? <div style={{display: 'flex', justifyContent: 'center', marginTop: 50}}><Loader /></div>
       : <PostList remove={removePost} posts={sortedAndSearchedPosts} title={'Post List 1'}/>
       }
-      
+      <div className="page__wrapper">
+        {pagesArray.map(p => {
+          return <span
+            onClick={() => changePage(p)}
+            key={p}
+            className={page === p ? 'page page__current' : 'page'}
+            
+            >
+              {p}
+            </span>
+        })}
+      </div>
+
     </div>
   );
 }
